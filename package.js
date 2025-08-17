fbacb46ed8636530986e815d48595da239f82304
Matthew Parker <matt_parker@outlook.com>
2025-08-17 15:15:11 -0400

    modulation fixes

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "geminibuddy-userscript",
+  "version": "36.0.0",
+  "description": "A modular userscript for Gemini, built with Webpack.",
+  "private": true,
+  "scripts": {
+    "build": "webpack --mode=production"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.24.7",
+    "@babel/preset-env": "^7.24.7",
+    "babel-loader": "^9.1.3",
+    "webpack": "^5.92.1",
+    "webpack-cli": "^5.1.4",
+    "webpack-userscript": "^3.2.1"
+  }
+}
